e5ee6f80c07b82c4f81b8a8bfb928c317d592ae6
fermi_dirac <ManueleBryan@gmail.com>
2018-01-21 14:01:24 -0800

    refactored tests

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ var eq = new EquationTree(
 				new EquationTree(3),
 				new EquationTree(EquationTree.operations.multiply,
 					new EquationTree(5),
-					new EquationTree(2))),
-			new EquationTree(9))
+					new EquationTree(2)
+				)
+			),
+			new EquationTree(9)
+		)
 	);
 
 console.log(eq)
